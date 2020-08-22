7abc1ee2df435ff25af7042b3cfc31f2b25179f3
gagik <gagik@princeton.edu>
2020-08-22 22:46:35 +0400

    Added basic structure.

---
+++ src/pages/components/layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/index.scss';
+import Footer from './footer';
+
+const Layout = (props) => {
+    return (
+        <div className="layout">
+            {props.children}
+            <Footer flipped={props.flipped} />
+        </div>
+    );
+}
+
+export default Layout;
