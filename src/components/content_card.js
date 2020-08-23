b3220c1c0f8314de63d551602e2039700067cb76
Daniel Wey <danw.6007@gmail.com>
2020-08-23 11:15:56 -0400

    content in learn.js, string literals should be moved, montserrat buggy

--- src/components/content_card.js
+++ src/components/content_card.js
@@ -1,41 +1,41 @@
 import React from 'react'
 import style from './content_card.module.scss'
 
+function showTitle(title, color) {
+    if (title) return (
+        <div className={style.title_section}>
+              <h1>{title}</h1>
+              <svg
+                width="87"
+                height="84"
+                viewBox="0 0 87 84"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  width="59.6249"
+                  height="59.6249"
+                  rx="5"
+                  transform="matrix(0.719782 -0.694201 0.719782 0.694201 0.38147 41.7334)"
+                  fill={color}
+                />
+                <rect
+                  width="19.875"
+                  height="19.875"
+                  rx="5"
+                  transform="matrix(0.719782 0.694201 -0.719782 0.694201 43.0107 27.6582)"
+                  fill="white"
+                />
+              </svg>
+            </div>
+    )
+}
+
 const ContentCard = props => {
   return (
     <div className={style.container}>
-      <div className={style.title_section}>
-        <h1>Placeholder</h1>
-        <svg
-          width="87"
-          height="84"
-          viewBox="0 0 87 84"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            width="59.6249"
-            height="59.6249"
-            rx="5"
-            transform="matrix(0.719782 -0.694201 0.719782 0.694201 0.38147 41.7334)"
-            fill={props.color}
-          />
-          <rect
-            width="19.875"
-            height="19.875"
-            rx="5"
-            transform="matrix(0.719782 0.694201 -0.719782 0.694201 43.0107 27.6582)"
-            fill="white"
-          />
-        </svg>
-      </div>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam iusto
-        ipsum quia deserunt? Minima blanditiis perferendis accusamus deleniti
-        aperiam non tempore numquam amet, architecto laborum aut nam, tenetur
-        adipisci quos.
-      </p>
+      {showTitle(props.title, props.color)}
+      <p>{props.text}</p>
     </div>
   )
 }
